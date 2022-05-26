a375aa30e1af529ea4a00a7bd6f8635208f4d014
Nikolai <nikolayizgarshev@yandex.ru>
2022-05-26 10:25:21 +0300

    Input, style, news

--- public/js/application.js
+++ public/js/application.js
@@ -1,21 +1,45 @@
 const searchForm = document.querySelector('.searchForm');
 console.log(searchForm);
 
-const input = document.getElementById('text');
-console.log(input);
+const divContainerNews = document.querySelector('.container-news')
 
 const API_KEY = '969ef0c663d34114b912c8c5216803b3';
 
 searchForm?.addEventListener('submit', async (event) => {
   event.preventDefault();
-  const word = input.value;
+  divContainerNews.innerHTML = ""
+  const word = event.target.word.value;
   console.log(word);
   const response = await fetch(`https://newsapi.org/v2/everything?q=${word}&from=2022-05-23&sortBy=popularity&language=ru&pageSize=10`, {
     headers: {
       'X-Api-Key': API_KEY,
     },
-    // body: JSON.stringify({}),
   });
+  console.log(11111, response);
   const result = await response.json();
-  console.log(result);
+  console.log(result.articles);
+  console.log(result.articles[0].title);
+  if(response.ok) {
+    for (let i = 0; i < result.articles.length; i++) {
+      divContainerNews.innerHTML += `
+       <div class="articleCard">
+        <h3>${result.articles[i].title}</h3>
+         <img src="${result.articles[i].urlToImage}" width="100%" />
+         <p>${result.articles[i].description}</p>
+         <a href="${result.articles[i].url}">Перейти к источнику</a>
+       </div>
+        `
+    }
+    event.target.reset()
+  }
+  // if(result.ok) {
+  //   divContainerNews.innerHTML = `
+  //   <div class="articleCard">
+  //   <h3>${{result.title}}</h3>
+  //   <img src="${{result.urlToImage}}" width="100%" />
+  //   <p>${{result.description}}</p>
+  //   <a href="{{this.url}}">Перейти к источнику</a>
+  // </div>
+  //   `
+  // }
 });
--- routes/main.js
+++ routes/main.js
@@ -4,4 +4,13 @@ router.get('/', (req, res) => {
   res.render('entries/mainPage');
 });
 
+router.post('/word', (req,res) => {
+  try {
+    res.status(200).send()
+  } catch (err) {
+    res.status(500).send()
+  }
+})
+
+
 module.exports = router;
