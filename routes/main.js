c356ea10e05e39a278f98247d457ccc17ffc69fe
Milana <milanammk@gmail.com>
2022-05-26 19:40:11 +0300

    BD words, userwords, fetch

--- routes/main.js
+++ routes/main.js
@@ -1,16 +1,37 @@
 const router = require('express').Router();
+const { User, Word, UserWord } = require('../db/models')
 
 router.get('/', (req, res) => {
-  res.render('entries/mainPage');
+	res.render('entries/mainPage');
 });
 
-router.post('/word', (req,res) => {
-  try {
-    res.status(200).send()
-  } catch (err) {
-    res.status(500).send()
-  }
+router.post('/word', async (req, res) => {
+	const { word, wordException } = req.body;
+	try {
+		if (word !== "") {
+			const wordInDb = await Word.findOne({ where: { name: word } });
+			console.log(wordInDb);
+			if (wordInDb) {
+				const newCounter = counter(wordInDb.counter);
+				await Word.update(
+					{ counter: newCounter },
+					{ where: { name: word } }
+				);
+			} else {
+				const newNote = await Word.create({ name: word, status: true, counter: '1' });
+				await UserWord.create({ user_id: req.session.user.id, word_id: newNote.id })
+			}
+		}
+		res.status(200).send()
+	} catch (err) {
+		res.status(500).send()
+	}
 })
 
+function counter(str) {
+	let num = Number(str);
+	num++;
+	return String(num);
+}
 
 module.exports = router;
