ff93ed6b54ad51d57946517faa53689288920c9d
Milana <milanammk@gmail.com>
2022-05-26 14:18:40 +0300

    new styles, new input, application.js

--- public/js/application.js
+++ public/js/application.js
@@ -9,46 +9,28 @@ searchForm?.addEventListener('submit', async (event) => {
 	divContainerNews.innerHTML = ""
 	const word = event.target.word.value;
 	const wordException = event.target.wordException.value;
+	let response;
 
 	if (wordException === "") {
-		const response = await fetch(`https://newsapi.org/v2/everything?q=${word}&from=2022-05-23&sortBy=popularity&language=ru&pageSize=10`, {
+		response = await fetch(`https://newsapi.org/v2/everything?q=${word}&from=2022-05-23&sortBy=popularity&language=ru&pageSize=10`, {
 			headers: {
 				'X-Api-Key': API_KEY,
 			},
 		});
-		console.log(11111, response);
-		const result = await response.json();
-		console.log(result.articles);
-		console.log(22222, result.articles[0]?.title);
-		if (response.ok && result.articles.length !== 0) {
-			for (let i = 0; i < result.articles.length; i++) {
-				divContainerNews.innerHTML += `
-       <div class="articleCard">
-        <h3>${result.articles[i].title}</h3>
-         <img src="${result.articles[i].urlToImage}" width="100%" />
-         <p>${result.articles[i].description}</p>
-         <a href="${result.articles[i].url}">Перейти к источнику</a>
-       </div>
-        `
-			}
-			event.target.reset()
-		} else {
-			divContainerNews.innerHTML = `<h1>Всё поломалось</h1>`
-		}
 	} else {
-
-		const response = await fetch(`https://newsapi.org/v2/everything?q=${word} NOT ${wordException}&from=2022-05-23&sortBy=popularity&language=ru&pageSize=10`, {
+		response = await fetch(`https://newsapi.org/v2/everything?q=${word} NOT ${wordException}&from=2022-05-23&sortBy=popularity&language=ru&pageSize=10`, {
 			headers: {
 				'X-Api-Key': API_KEY,
 			},
 		});
-		console.log(11111, response);
-		const result = await response.json();
-		console.log(result.articles);
-		console.log(22222, result.articles[0]?.title);
-		if (response.ok && result.articles.length !== 0) {
-			for (let i = 0; i < result.articles.length; i++) {
-				divContainerNews.innerHTML += `
+	}
+	console.log(11111, response);
+	const result = await response.json();
+	console.log(result.articles);
+	console.log(22222, result.articles[0]?.title);
+	if (response.ok && result.articles.length !== 0) {
+		for (let i = 0; i < result.articles.length; i++) {
+			divContainerNews.innerHTML += `
        <div class="articleCard">
         <h3>${result.articles[i].title}</h3>
          <img src="${result.articles[i].urlToImage}" width="100%" />
@@ -56,10 +38,9 @@ searchForm?.addEventListener('submit', async (event) => {
          <a href="${result.articles[i].url}">Перейти к источнику</a>
        </div>
         `
-			}
-			event.target.reset()
-		} else {
-			divContainerNews.innerHTML = `<h1>Всё поломалось</h1>`
 		}
+		event.target.reset()
+	} else {
+		divContainerNews.innerHTML = `<h1>Всё поломалось</h1>`
 	}
 });
